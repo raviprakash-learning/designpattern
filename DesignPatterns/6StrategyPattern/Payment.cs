c9ffb43e3ede7cc05b7ca29e1a93ab7d586833ab
Ravi Prakash <raviprakash.learning@gmail.com>
2023-03-12 01:03:48 +0530

    1.Strategy pattern Example

---
+++ DesignPatterns/6StrategyPattern/Payment.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6StrategyPattern
+{
+    public abstract class Payment
+    {
+        public PaymentBehavior paymentBehavior;
+        // common method will stay here and method which behavior which is not same to each client move
+        // that to new behaviour ex we move paymentBehavuour to separate interface
+        public void PerformPayment()
+        {
+            paymentBehavior.MakePayment();
+        }
+    }
+
+    public interface PaymentBehavior
+    {
+        public void MakePayment();
+    }
+
+    public class CreditCardPaymentBehavior : PaymentBehavior
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Using Credit Card");
+        }
+    }
+    public class UPIPaymentBehavior : PaymentBehavior
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Using UPI");
+        }
+    }
+    public class WalletPaymentBehavior : PaymentBehavior
+    {
+        public void MakePayment()
+        {
+            Console.WriteLine("Payment Using Wallet");
+        }
+    }
+
+    public class CreditCardPayment : Payment
+    {
+        public CreditCardPayment()
+        {
+            paymentBehavior = new CreditCardPaymentBehavior();
+        }
+    }
+
+    public class UPIPayment : Payment
+    {
+        public UPIPayment()
+        {
+            paymentBehavior = new UPIPaymentBehavior();
+        }
+    }
+    public class WalletPayment : Payment
+    {
+        public WalletPayment()
+        {
+            paymentBehavior = new WalletPaymentBehavior();
+        }
+    }
+
+
+}
---
+++ DesignPatterns/6StrategyPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Payment payment = new CreditCardPayment();
+            payment.PerformPayment();
+
+            payment = new UPIPayment();
+            payment.PerformPayment();
+
+
+            payment = new WalletPayment();
+            payment.PerformPayment();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
